4de69e4e927bdf54852005568ea15e8853d0fd31
963974796 <963974796@qq.com>
2019-03-12 15:09:53 +0800

    第一次

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class IndexController extends Controller
+{
+    public function index(Request $request){
+        $request=$request->input('name');
+        $users = DB::table('test')->get();
+        dd($users);
+    }
+}
