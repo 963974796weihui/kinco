66e057629fabc4dcf6cd71d55f68ad032845e7bd
963974796 <963974796@qq.com>
2019-03-12 17:24:29 +0800

    phpstrom提交

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Services\Admin\IndexServicesController;
 
 class IndexController extends Controller
 {
-    public function index(Request $request){
-        $request=$request->input('name');
-        $users = DB::table('test')->get();
-        dd($users);
+    public function index(IndexServicesController $IndexServicesController,Request $request){
+        $request=$request->all();
+        $result=$IndexServicesController->registerServices($request);
+        if(!is_array($result)) {
+            return response()->json(['status' => 'F', 'code' => '201', 'message' => $result]);
+        }
+        $request['time']=time();
+        $res=DB::table('ki_administrtor')->insert($request);
+        dd($res);
     }
 }
