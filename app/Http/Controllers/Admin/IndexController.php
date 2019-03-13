68d869144b49849c87667e26e772f7d5fc2b7ab2
963974796 <963974796@qq.com>
2019-03-13 16:56:58 +0800

    phpstrom提交

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Services\Admin\IndexServicesController;
+use Illuminate\Support\Facades\Mail;
 
 class IndexController extends Controller
 {
@@ -16,7 +17,13 @@ class IndexController extends Controller
             return response()->json(['status' => 'F', 'code' => '201', 'message' => $result]);
         }
         $request['time']=time();
-        $res=DB::table('ki_administrtor')->insert($request);
-        dd($res);
+        $request['register_confirm_code']=str_random(32);
+        $res=DB::table('ki_administrtor')->insertGetId($request);
+        if($res){
+            return response()->json(['status' => 'S', 'code' => '200', 'message' => '注册成功']);
+        }
+//        $registerCode['id']=$res;
+//        $registerCode['register_confirm_code']=$request['register_confirm_code'];
+//        $mail=$IndexServicesController->mailSend($registerCode);//发送邮件
     }
 }
