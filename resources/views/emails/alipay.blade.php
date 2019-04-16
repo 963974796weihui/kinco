84857155339559f317b8072d54e9c7a4cd3e650a
963974796 <963974796@qq.com>
2019-04-16 15:36:06 +0800

    修改工程

--- resources/views/emails/alipay.blade.php
+++ resources/views/emails/alipay.blade.php
@@ -1,2 +1,17 @@
-{{$data[0]->out_trade_no}}
+<br>
+您购买的授权码:{{$data[0]->sncode}}
+<br>
+有效期:90天
+<br>
+购买时间:<?php echo date('Y-m-d','{{$data[0]->buy_time}}');?>
+<br>
+支付方式:支付宝
+<br>
+支付标题:{{$data[0]->subject}}}
+<br>
+交易号:{{$data[0]->out_trade_no}}}
+<br>
+订单号:{{$data[0]->trade_no}}}
+<br>
+支付金额:{{$data[0]->total_amount}}}
 <br>
