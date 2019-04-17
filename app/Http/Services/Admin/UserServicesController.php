6b2583c335b125674728600af8d79c152d930ce0
963974796 <963974796@qq.com>
2019-04-17 15:30:00 +0800

    aa

--- app/Http/Services/Admin/UserServicesController.php
+++ app/Http/Services/Admin/UserServicesController.php
@@ -164,10 +164,10 @@ class UserServicesController extends Controller
 
     public function addUser($request)
     {
-        $id = DB::table('ki_admin_user')->where('user_name', '=', $request['user_name'])->get()->toArray();
-        if ($id) {
-            return false;
-        }
+//        $id = DB::table('ki_admin_user')->where('user_name', '=', $request['user_name'])->get()->toArray();
+//        if ($id) {
+//            return false;
+//        }
         $request['password'] = str_random(6);
         $request['register_confirm_code'] = str_random(32);
         $request['time'] = time();
