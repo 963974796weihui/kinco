90ad0c68588a9ca951f5872e17aa75155fd717a9
963974796 <963974796@qq.com>
2019-04-19 16:53:15 +0800

    aa

--- app/Http/Services/Admin/UserServicesController.php
+++ app/Http/Services/Admin/UserServicesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Services\Admin;
     use App\Http\Controllers\Controller;
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\DB;
+    use Illuminate\Support\Facades\Mail;
 
 class UserServicesController extends Controller
 {
